4ede71cbe38851bb4784f5300f8584cd2df10727
MaxiDotti <dottimaximo@hotmail.com>
2023-05-30 20:44:30 -0300

    Creacion de la clase Socio, constructores, setter y getter

---
+++ src/main/java/org/Models/Socio.java
@@ -0,0 +1,19 @@
+package org.Models;
+
+public class Socio extends Usuario{
+    private boolean estadoContable; /// DEUDOR o AL DIA
+
+    public Socio(String nombre, String apellido, String dni, String contrasenia, String telefono, String direccion, boolean estadoContable) {
+        super(nombre, apellido, dni, contrasenia, telefono, direccion);
+        this.estadoContable = estadoContable;
+    }
+    public Socio(){}
+
+    public boolean isEstadoContable() {
+        return estadoContable;
+    }
+
+    public void setEstadoContable(boolean estadoContable) {
+        this.estadoContable = estadoContable;
+    }
+}
