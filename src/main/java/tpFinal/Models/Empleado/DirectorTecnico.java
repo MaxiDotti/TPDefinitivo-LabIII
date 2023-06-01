6f6757333da1c5497673782cebe8d7d0ce4b576c
JuanmaBadra <juanmabadra@gmail.com>
2023-06-01 13:09:54 -0300

    Se actualizo clase DirectorTecnico

--- src/main/java/tpFinal/Models/Empleado/DirectorTecnico.java
+++ src/main/java/tpFinal/Models/Empleado/DirectorTecnico.java
@@ -5,13 +5,18 @@ import tpFinal.Models.Usuario;
 public class DirectorTecnico extends Usuario {
 
     private boolean estadoContable;
-
     private String plantel;
+    private String deporte;
+    private String categoria;
 
-    public DirectorTecnico(boolean estadoContable, String plantel) {
+    public DirectorTecnico(boolean estadoContable, String plantel, String deporte, String categoria) {
         this.estadoContable = estadoContable;
         this.plantel = plantel;
+        this.deporte = deporte;
+        this.categoria = categoria;
     }
+
+    public DirectorTecnico(){}
 
     public boolean isEstadoContable() {
         return estadoContable;
@@ -29,6 +34,22 @@ public class DirectorTecnico extends Usuario {
         this.plantel = plantel;
     }
 
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
     void verEstadoContable(){
         if(this.estadoContable){
             System.out.println("Usted a cobrado.");
@@ -36,6 +57,5 @@ public class DirectorTecnico extends Usuario {
             System.out.println("Aun no posee saldo.");
         }
     }
-
 
 }
