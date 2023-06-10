e63ac84dc7dcaac0fa883d258269ebf05b3457f4
MaxiDotti <dottimaximo@hotmail.com>
2023-06-10 18:25:10 -0300

    Creacion de clase SocioService e interface ISocioService

---
+++ src/main/java/tpFinal/Services/SocioService.java
@@ -0,0 +1,57 @@
+package tpFinal.Services;
+
+import tpFinal.Models.Socio;
+import tpFinal.Repositorios.SociosRepository;
+
+public class SocioService implements ISocioService{
+    SociosRepository sociosRepo = new SociosRepository();
+
+    @Override
+    public void listar() {
+        sociosRepo.listar().forEach(System.out::println);
+    }
+
+    @Override
+    public void agregar(Socio objeto) {
+        sociosRepo.agregar(objeto);
+    }
+
+    @Override
+    public void modificar(String dni) {
+        sociosRepo.modificar(dni);
+    }
+
+    @Override
+    public void eliminar() {
+
+    }
+
+    @Override
+    public boolean buscarSocio(String dni) {
+        if(sociosRepo.buscarSocio(dni)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void comprarEntrada() {
+
+    }
+
+    @Override
+    public void comprarMerchandasing() {
+
+    }
+
+    @Override
+    public void verEstadoContable() {
+
+    }
+
+    @Override
+    public void listarBeneficios() {
+
+    }
+}
