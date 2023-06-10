04ab5e0cc18286f8548e8479611b0a356ef874cf
MaxiDotti <dottimaximo@hotmail.com>
2023-06-10 19:05:50 -0300

    Mejora en el metodo modificar

--- src/main/java/tpFinal/Repositorios/SociosRepository.java
+++ src/main/java/tpFinal/Repositorios/SociosRepository.java
@@ -9,12 +9,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 public class SociosRepository implements IRepository<Socio> {
     private final File file = new File("src/main/resources/Socios.json");
     private final ObjectMapper mapper = new ObjectMapper();
     private List<Socio> listaSocios;
-
-
 
     @Override
     public void cargar() {
@@ -65,8 +65,75 @@ public class SociosRepository implements IRepository<Socio> {
        guardar();
     }
 
+    public boolean buscarSocio(String dni){ // RETORNA TRUE SI EL SOCIO EXISTE EN EL ARCHIVO
+        cargar();
+        for(Socio socio : this.listaSocios){
+            if(socio.getDni() == dni){
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void modificar(String dni) {
+        cargar();
+        for(Socio socio : this.listaSocios){
+            if(socio.getDni() == dni){
+                Socio modificado = modificarAux();
+                socio.setContrasenia(modificado.getContrasenia());
+                socio.setTelefono(modificado.getTelefono());
+                socio.setDireccion(modificado.getDireccion());
+            }
+        }
+        guardar();
+    }
 
+    public Socio modificarAux(){
+        Socio nuevo = new Socio();
+
+        Scanner leer = new Scanner(System.in);
+        String cambio; // USO ESTA VARIABLE PARA GUARDAR LOS CAMBIOS SI ASI EL SOCIO LO DESEA
+
+        System.out.print("MODIFICAR CONTRASEÑA? 1 SI - 2 NO: ");
+        int opcion = leer.nextInt();
+
+        if(opcion == 1){
+            System.out.print("NUEVA CONTRASEÑA: ");
+            cambio = leer.next();
+
+            while(opcion == 1) {
+                System.out.print("INGRESE NUEVAMENTE LA CONTRASEÑA: ");
+                String cambio2 = leer.next();
+                if (cambio == cambio2) {
+                    nuevo.setContrasenia(cambio2);
+                    System.out.println("LA CONTRASEÑA FUE MODIFICADA CORRECTAMENTE.");
+                    opcion = -1;
+                } else {
+                    System.out.println("LA CONTRASEÑA INGRESADA NO COINCIDE. INTENTE NUEVAMENTE.");
+                }
+            }
+        }
+
+        System.out.print("MODIFICAR TELEFONO? 1 SI - 2 NO: ");
+        opcion = leer.nextInt();
+
+        if(opcion == 1){
+            System.out.print("INGRESE EL TELEFONO: ");
+            cambio = leer.next();
+            nuevo.setTelefono(cambio);
+            System.out.println("EL TELEFONO FUE MODIFICADO CORRECTAMENTE.");
+        }
+
+        System.out.print("MODIFICAR DIRECCION? 1 SI - 2 NO: ");
+        opcion = leer.nextInt();
+
+        if(opcion == 1){
+            System.out.print("INGRESE LA DIRECCION: ");
+            cambio = leer.next();
+            nuevo.setDireccion(cambio);
+            System.out.println("LA DIRECCION FUE MODIFICADA CORRECTAMENTE.");
+        }
+        return nuevo;
     }
 }
