1585fab3b2650ce069a0404524aae8b55788783a
MaxiDotti <dottimaximo@hotmail.com>
2023-06-10 21:16:21 -0300

    Metodos de JugadorService

--- src/main/java/tpFinal/Services/IJugadorService.java
+++ src/main/java/tpFinal/Services/IJugadorService.java
@@ -1,15 +1,17 @@
 package tpFinal.Services;
 
+import tpFinal.Models.Empleado.Jugador;
+
 public interface IJugadorService {
-    void listarJugadorPorCategoria(); // SE DEBE SELECCIONAR DEPORTE
-    void listarTodosLosJugadores(); // SE DEBE SELECCIONAR DEPORTE
-    void listarPorPosicionYCategoria(); // SE DEBE SELECCIONAR DEPORTE
-    void agregarJugador(); // SOLO UTILIZA ADMIN O DIRECTIVO
-    void modificarJugador(); // SOLO UTILIZA ADMIN O DIRECTIVO
-    void eliminarJugador(); // SOLO UTILIZA ADMIN O DIRECTIVO
+    void listarJugadorPorCategoria(String deporte, String categoria); // SE DEBE SELECCIONAR DEPORTE
+    void listarTodosLosJugadores(String deporte); // SE DEBE SELECCIONAR DEPORTE
+    void listarPorPosicionYCategoria(String deporte, String categoria, String posicion); // SE DEBE SELECCIONAR DEPORTE
+    void agregarJugador(Jugador nuevo); // SOLO UTILIZA ADMIN O DIRECTIVO
+    void modificarJugador(String dni); // SOLO UTILIZA ADMIN O DIRECTIVO
+    void eliminarJugador(String dni); // SOLO UTILIZA ADMIN O DIRECTIVO
     void cambiarCategoria();
     void cambiarDeporte();
     void cambiarPosicio();
-    boolean estadoContable();
+    boolean estadoContable(String dni);
 
 }
